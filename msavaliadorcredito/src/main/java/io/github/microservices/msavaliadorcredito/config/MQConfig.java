72e4a7911af768367584cf790117bb978257bb97
manoel.junior <manoel.junior@tceal.tc.br>
2022-12-30 13:56:04 -0300

    Desenvolvendo RabbitMQ nos microservices mscartoes e msavaliadorcredito

--- msavaliadorcredito/src/main/java/io/github/microservices/msavaliadorcredito/config/MQConfig.java
+++ msavaliadorcredito/src/main/java/io/github/microservices/msavaliadorcredito/config/MQConfig.java
@@ -3,14 +3,16 @@ package io.github.microservices.msavaliadorcredito.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MQConfig {
 
-    @Value("${mg.queues.emissao-cartoes}")
+    @Value("${mq.queues.emissao-cartoes}")
     private String emissaoCartoesFila;
 
+    @Bean
     public Queue queueEmissaoCartoes() {
         return new Queue(emissaoCartoesFila, true);
     }
