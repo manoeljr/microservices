25bcf0d48ce7357b5aaa68d2a11584c25b476955
manoel.junior <manoel.junior@tceal.tc.br>
2023-01-10 13:49:16 -0300

    Adicionando login e senha no Eureka e logs

--- msclientes/src/main/java/io/github/microservices/msclientes/application/ClienteResource.java
+++ msclientes/src/main/java/io/github/microservices/msclientes/application/ClienteResource.java
@@ -5,6 +5,8 @@ import io.github.microservices.msclientes.application.representation.ClienteSave
 import io.github.microservices.msclientes.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
